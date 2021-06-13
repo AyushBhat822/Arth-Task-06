9bccd04d5272523043c6af41b802d194ffe57bf8
Ayush Bhat <67255324+AyushBhat822@users.noreply.github.com>
2021-06-13 22:52:33 +0530

    Update block.tf

--- block.tf
+++ block.tf
@@ -1,6 +1,6 @@
 resource "aws_ebs_volume" "st1" {
   availability_zone = aws_instance.webos1.availability_zone
-  size              = 1
+  size              = 5
 
   tags = {
     Name = "Web Server HD new by TF"
